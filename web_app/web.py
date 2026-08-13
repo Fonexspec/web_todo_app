11c56fc18cc20b05f39eb359fd9ee43cb2e075da
Funsho <Fonexspec@gmail.com>
2026-08-13 22:48:57 +0100

    changed the whole code again

--- web_app/web.py
+++ web_app/web.py
@@ -6,47 +6,22 @@ todos = functions.get_todos()
 
 
 def add_todo():
-    todo = st.session_state["new_todo"].strip()
-
-    if todo:
-        todos.append(todo + "\n")
-        functions.write_todos(todos)
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
 
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-
-# Display todos
-completed_todos = []
-
 for index, todo in enumerate(todos):
-    todo_text = todo.strip()
-
-    checkbox = st.checkbox(
-        todo_text,
-        key=f"todo_{index}"
-    )
-
+    checkbox = st.checkbox(todo, key=todo)
     if checkbox:
-        completed_todos.append(index)
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
-
-# Remove completed todos after the loop
-if completed_todos:
-    todos = [
-        todo for index, todo in enumerate(todos)
-        if index not in completed_todos
-    ]
-
-    functions.write_todos(todos)
-    st.rerun()
-
-
-st.text_input(
-    label="",
-    placeholder="Add new todo...",
-    on_change=add_todo,
-    key="new_todo"
-)
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key="new_todo")
