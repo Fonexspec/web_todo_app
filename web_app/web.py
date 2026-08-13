b3078f4a40fafff0ed1f7d80a0a7701e2f305f34
Funsho <Fonexspec@gmail.com>
2026-08-13 22:19:03 +0100

    changed the whole code

--- web_app/web.py
+++ web_app/web.py
@@ -6,22 +6,47 @@ todos = functions.get_todos()
 
 
 def add_todo():
-    todo = st.session_state["new_todo"] + "\n"
-    todos.append(todo)
-    functions.write_todos(todos)
+    todo = st.session_state["new_todo"].strip()
+
+    if todo:
+        todos.append(todo + "\n")
+        functions.write_todos(todos)
 
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=str(index))
-    if checkbox:
-        todos.pop(index)
-        functions.write_todos(todos)
-        del st.session_state[todo]
-        st.rerun()
 
-st.text_input(label="", placeholder="Add new todo...",
-              on_change=add_todo, key="new_todo")
+# Display todos
+completed_todos = []
+
+for index, todo in enumerate(todos):
+    todo_text = todo.strip()
+
+    checkbox = st.checkbox(
+        todo_text,
+        key=f"todo_{index}"
+    )
+
+    if checkbox:
+        completed_todos.append(index)
+
+
+# Remove completed todos after the loop
+if completed_todos:
+    todos = [
+        todo for index, todo in enumerate(todos)
+        if index not in completed_todos
+    ]
+
+    functions.write_todos(todos)
+    st.rerun()
+
+
+st.text_input(
+    label="",
+    placeholder="Add new todo...",
+    on_change=add_todo,
+    key="new_todo"
+)
