d6240d5eac5cf208896a4144ae45ecaa97c65ac9
Funsho <Fonexspec@gmail.com>
2026-08-13 19:38:22 +0100

    initial commit

---
+++ todowebapp/functions.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+FILEPATH = Path(__file__).parent / "todos.txt"
+
+
+def get_todos(file_path=FILEPATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(file_path, "r") as file:
+        todos_local = file.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, file_path=FILEPATH):
+    """ Write the to-do items list to a text file. """
+    with open(file_path, "w") as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
