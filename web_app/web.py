9f39110e741d80487b1996875b22fb8ccf3a62db
Funsho <Fonexspec@gmail.com>
2026-08-13 22:02:32 +0100

    Updated fix error checkbox fix.

--- web_app/web.py
+++ web_app/web.py
@@ -16,7 +16,7 @@ st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+    checkbox = st.checkbox(todo, key=str(index))
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
